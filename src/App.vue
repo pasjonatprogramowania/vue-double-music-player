f822aa5d197c2a382da6831000ac653e0d2c00a0
PasjonatProgramowania <paw.szew1234@gmail.com>
2022-01-25 20:25:20 +0100

    new version with history

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <div id="app" class="video">
     <div v-for="(video, index) in videosUrl" :key="index">
       <div class="input-container">
+        <button @click="videosUrl[0].url = ''">Usuń</button>
         <label for="first-video-url">Link url </label>
-        <input type="text" name="first-video" v-model="video.url" />
+        <input
+          @blur="getTitle()"
+          type="text"
+          name="first-video"
+          v-model="video.url"
+        />
       </div>
       <div>
         <youtube
@@ -14,33 +20,75 @@
         ></youtube>
       </div>
     </div>
+    <div v-for="(video, i) in watechedVideos" :key="`video-${i}`">
+      <p>
+        {{ video }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
 import VueYouTubeEmbed from "vue-youtube-embed";
+import getYoutubeTitle from "get-youtube-title";
 Vue.use(VueYouTubeEmbed);
 
 import { getIdFromURL } from "vue-youtube-embed";
 
 export default {
   name: "App",
-
   data() {
     return {
-      videosUrl: [{ url: "" }, { url: "" }],
+      title: "",
+      videosUrl: [
+        { url: "" },
+        { url: "https://www.youtube.com/watch?v=e3KpJ5k1Htg" },
+      ],
+      watechedVideos: [],
     };
+  },
+  mounted() {
+    this.fetchWachedVideos();
   },
   methods: {
     returnFirstVideoId(aUrl) {
       return getIdFromURL(aUrl);
+    },
+    fetchWachedVideos() {
+      let localStorageData = localStorage.getItem("watchedVideos");
+      if (localStorageData) this.watechedVideos = JSON.parse(localStorageData);
+    },
+    async getTitle() {
+      let id = getIdFromURL(this.videosUrl[0].url);
+      await getYoutubeTitle(id, (e, t) => {
+        if (e) console.error(e);
+        this.addNewVideoToLocalStorage(t);
+      });
+    },
+    addNewVideoToLocalStorage(t) {
+      let localStorageData = localStorage.getItem("watchedVideos");
+      if (localStorageData) {
+        let arr = JSON.parse(localStorageData);
+        arr.push(t);
+        localStorage.setItem("watchedVideos", JSON.stringify(arr));
+      } else {
+        let arr = [];
+        arr.push(t);
+        localStorage.setItem("watchedVideos", JSON.stringify(arr));
+      }
     },
   },
 };
 </script>
 
 <style lang="sass">
+button
+  background-color: #2d3436
+  color: #dfe6e9
+  font-size: 1.4rem
+  width: 100%
+  height: 40px
 @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;1,400&display=swap')
 html
   font-family: 'Lato', sans-serif
@@ -54,7 +102,7 @@ svg
 .input-container
   padding: 30px 0 30px 0
 
-label
+label,p
   color: #dfe6e9
   font-size: 1.4rem
 input
