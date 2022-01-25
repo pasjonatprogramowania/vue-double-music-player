0f77f93d4afa9de930bfd7536ce575f56e60fe37
pasjonatprogramowania <44599809+pasjonatprogramowania@users.noreply.github.com>
2022-01-25 20:29:37 +0100

    ddd

---
+++ precache-manifest.176e7a8b8f8e51f14a6cb667340c5b78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ef4d550b4636ea53c1",
+    "url": "/vue-double-music-player/css/app.863c3667.css"
+  },
+  {
+    "revision": "a1a92a44c213cbb47686895711408268",
+    "url": "/vue-double-music-player/index.html"
+  },
+  {
+    "revision": "a1ef4d550b4636ea53c1",
+    "url": "/vue-double-music-player/js/app.4fe331be.js"
+  },
+  {
+    "revision": "7945e4662087ca6cfc85",
+    "url": "/vue-double-music-player/js/chunk-vendors.c502c754.js"
+  },
+  {
+    "revision": "5917ac2c99989c5f949f96f449cd1ac5",
+    "url": "/vue-double-music-player/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-double-music-player/robots.txt"
+  }
+]);
